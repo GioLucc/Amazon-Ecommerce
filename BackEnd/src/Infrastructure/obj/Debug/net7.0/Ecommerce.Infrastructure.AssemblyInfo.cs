4cf3c774b0969fd67db234b18b9b4af8e922294d
Giovanni <giolucarna@gmail.com>
2024-08-14 04:44:13 -0300

    Feat :fire: - Remove bin and obj directories from Git tracking

--- BackEnd/src/Infrastructure/obj/Debug/net7.0/Ecommerce.Infrastructure.AssemblyInfo.cs
+++ BackEnd/src/Infrastructure/obj/Debug/net7.0/Ecommerce.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddec5132559992ac16abe88c6ab98608295d4a2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ba95a22152d19a766946d6060fc5d6c97449826")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
